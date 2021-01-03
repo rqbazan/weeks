eed72a7a8168aa795c157a6b2ed7153ef93ddde3
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2021-01-02 23:29:26 -0500

    feat: add sticky (#1)

--- src/pages/[country]/[birthdate].js
+++ src/pages/[country]/[birthdate].js
@@ -24,7 +24,7 @@ function Cell({ borderful, disabled, className, styles, children }) {
     <span
       style={styles}
       className={clsx(
-        'flex w-4 h-4 text-gray-300 flex-shrink-0 font-mono',
+        'flex w-4 h-4 flex-shrink-0 text-gray-300 font-mono',
         borderful && 'border rounded-sm',
         disabled
           ? ['bg-gray-700 text-gray-600', borderful && 'border-gray-600']
@@ -48,6 +48,49 @@ function Stat({ value, title }) {
   )
 }
 
+function Matrix({ userWeeks, lifeExpectancyWeeks }) {
+  return LIFE_IN_YEARS.map((_, x) => {
+    const isAMultipleOf10 = (x + 1) % 10 === 0
+
+    return (
+      <div
+        key={`row-${x}`}
+        className={clsx('flex justify-between', {
+          'mb-4': isAMultipleOf10,
+          'mb-1': !isAMultipleOf10,
+        })}
+      >
+        {WEEKS_IN_ONE_YEAR.map((_, y) => {
+          const week = WEEKS_IN_ONE_YEAR.length * x + y + 1
+
+          // percentage of cells that must be paint, from 0 to 1
+          const c1 = clamp(1 + (userWeeks - week), 0, 1)
+
+          // percentage of cells that are available to be paint, from 0 to 1
+          const c2 = clamp(1 + (lifeExpectancyWeeks - week), 0, 1)
+
+          return (
+            <Cell
+              key={`cell-${x}-${y}`}
+              borderful
+              className={clsx({
+                '$colorful-cell bg-gray-600': c1 !== 0,
+                '$available-cell bg-gray-700': c1 === 0 && c2 !== 0,
+                'mr-4': (y + 1) % 4 === 0 && y !== WEEKS_IN_ONE_YEAR.length - 1,
+              })}
+              disabled={c2 === 0}
+              styles={{
+                '--c1': `${c1 * 100}%`,
+                '--c2': `${c2 * 100}%`,
+              }}
+            />
+          )
+        })}
+      </div>
+    )
+  })
+}
+
 export default function IndexPage({
   userWeeks,
   lifeExpectancyWeeks,
@@ -62,7 +105,7 @@ export default function IndexPage({
         <header className="flex justify-center items-center py-4 md:py-8 text-2xl md:text-4xl">
           <h1 className="text-gray-300 font-heading">MY LIFE IN WEEKS</h1>
         </header>
-        <section className="flex flex-col md:flex-row items-center md:justify-center px-6 md:px-0 space-y-2 md:space-x-4 md:space-y-0 font-mono mb-3">
+        <section className="flex flex-col md:flex-row items-center md:justify-center px-6 md:px-0 space-y-2 md:space-x-4 md:space-y-0 font-mono mb-4">
           <Stat value={userWeeks} title="consumed weeks" />
           <div className="h-3 w-0.5 bg-gray-300 my-auto hidden md:block" />
           <Stat value={lifeExpectancyWeeks} title="expected weeks" />
@@ -72,8 +115,8 @@ export default function IndexPage({
             title="remaining weeks"
           />
         </section>
-        <div className="flex p-1 pr-2 md:p-4">
-          <div className="flex flex-col mr-2 pt-6">
+        <div className="flex p-1 pt-0 pr-2 md:p-4 md:pt-0">
+          <div className="flex flex-col mr-2 pt-8">
             {LIFE_IN_YEARS.map((_, i) => {
               const isAMultipleOf10 = (i + 1) % 10 === 0
 
@@ -82,7 +125,7 @@ export default function IndexPage({
                   key={`years-${i}`}
                   disabled={i + 1 > lifeExpectancyYears}
                   className={clsx('w-auto items-center justify-end', {
-                    'mb-4': isAMultipleOf10,
+                    'mb-4': isAMultipleOf10 && i !== LIFE_IN_YEARS.length - 1,
                     'mb-1': !isAMultipleOf10,
                   })}
                 >
@@ -91,59 +134,34 @@ export default function IndexPage({
               )
             })}
           </div>
-          <div className="flex flex-col flex-grow overflow-y-auto">
-            <header className="flex justify-between mb-2">
-              {WEEKS_IN_ONE_YEAR.map((_, i) => (
-                <Cell
-                  key={`header-${i}`}
-                  className={clsx('items-center justify-end', {
-                    'mr-4': (i + 1) % 4 === 0,
-                  })}
-                >
-                  {getNthByStep(i, 4)}
-                </Cell>
-              ))}
-            </header>
-            {LIFE_IN_YEARS.map((_, x) => {
-              const isAMultipleOf10 = (x + 1) % 10 === 0
-
-              return (
-                <div
-                  key={`row-${x}`}
-                  className={clsx('flex justify-between', {
-                    'mb-4': isAMultipleOf10,
-                    'mb-1': !isAMultipleOf10,
-                  })}
-                >
-                  {WEEKS_IN_ONE_YEAR.map((_, y) => {
-                    const week = WEEKS_IN_ONE_YEAR.length * x + y + 1
-
-                    // percentage of cells that must be paint, from 0 to 1
-                    const c1 = clamp(1 + (userWeeks - week), 0, 1)
-
-                    // percentage of cells that are available to be paint, from 0 to 1
-                    const c2 = clamp(1 + (lifeExpectancyWeeks - week), 0, 1)
-
-                    return (
-                      <Cell
-                        key={`cell-${x}-${y}`}
-                        borderful
-                        className={clsx({
-                          'mr-4': (y + 1) % 4 === 0,
-                          '$colorful-cell bg-gray-600': c1 !== 0,
-                          '$available-cell bg-gray-700': c1 === 0 && c2 !== 0,
-                        })}
-                        disabled={c2 === 0}
-                        styles={{
-                          '--c1': `${c1 * 100}%`,
-                          '--c2': `${c2 * 100}%`,
-                        }}
-                      />
-                    )
-                  })}
-                </div>
-              )
-            })}
+          <div className="flex flex-col flex-grow min-w-0">
+            <div className="flex items-end pb-2 h-8 sticky bg-gray-700 top-0 z-10">
+              <header
+                className="syncscroll flex w-full overflow-x-auto overflow-y-hidden justify-between $hidden-scrollbar"
+                name="scrollable-matrix"
+              >
+                {WEEKS_IN_ONE_YEAR.map((_, i) => (
+                  <Cell
+                    key={`header-${i}`}
+                    className={clsx('items-center justify-end', {
+                      'mr-4':
+                        (i + 1) % 4 === 0 && i !== WEEKS_IN_ONE_YEAR.length - 1,
+                    })}
+                  >
+                    {getNthByStep(i, 4)}
+                  </Cell>
+                ))}
+              </header>
+            </div>
+            <div
+              className="syncscroll flex flex-col w-full overflow-x-auto"
+              name="scrollable-matrix"
+            >
+              <Matrix
+                userWeeks={userWeeks}
+                lifeExpectancyWeeks={lifeExpectancyWeeks}
+              />
+            </div>
           </div>
         </div>
       </div>
